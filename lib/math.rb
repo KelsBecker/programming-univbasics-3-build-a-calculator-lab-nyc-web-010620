be9769c3d0e2a51198c947cc373454299380d151
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-11 00:36:00 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
-end
-
+  sum=num1+num2
+  sum
+ end
+ addition(5,4)
+ 
 def subtraction(num1, num2)
-
+  sum=num1-num2
+  sum
 end
+subtraction(10,5)
 
 def division(num1, num2)
-
+  sum=num1/num2
+  sum
 end
+division(50,2.0)
 
 def multiplication(num1, num2)
-
+  sum=num1*num2
+  sum
 end
+multiplication(4,30)
 
 def modulo(num1, num2)
-
+  sum=(num1%num2)
+  sum
 end
+modulo(34,5)
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
+square_root(81)
